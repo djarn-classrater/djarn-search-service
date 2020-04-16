36dc98ea3bec0c876f9d71572a5b7cc1640c7c6d
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-04-17 02:07:31 +0700

    Initial course module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { SearchModule } from './search/search.module'
 import { SearchController } from './search/search.controller'
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SearchModule],
+  imports: [ConfigModule.forRoot(), SearchModule, CourseModule],
   controllers: [AppController, SearchController],
   providers: [AppService],
 })
